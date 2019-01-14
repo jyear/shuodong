27710bda3cf68446eefd3d8ba4cbe51088e29ec2
yangxu <yx53074@hotmail.com>
2019-01-14 22:36:39 +0800

    222

--- js/flag.js
+++ js/flag.js
@@ -19,7 +19,40 @@ var time = setTime();
 var renderObj = {
     show: "step1",
     date: time,
-    showTab: "tab1"
+    showTab: "tab1",
+    tabData: [
+        {
+            text: "张三丰",
+            isChecked: false,
+            id: 0
+        },
+        {
+            text: "张三丰",
+            isChecked: false,
+            id: 0
+        },
+        {
+            text: "张三丰",
+            isChecked: false,
+            id: 0
+        },
+        {
+            text: "张三丰",
+            isChecked: false,
+            id: 0
+        },
+        {
+            text: "张三丰",
+            isChecked: false,
+            id: 0
+        },
+        {
+            text: "张三丰",
+            isChecked: false,
+            id: 0
+        }
+    ],
+    checkedData: []
 };
 var newObject = {};
 var lookRenderObj = function() {
@@ -30,7 +63,7 @@ var lookRenderObj = function() {
                 $("#" + obj[key]).addClass("show");
                 break;
             case "showTab":
-                $(".page").removeClass("show");
+                $(".tab").removeClass("show");
                 $("#" + obj[key]).addClass("show");
                 break;
             case "date":
@@ -38,6 +71,50 @@ var lookRenderObj = function() {
                 for (var k in date) {
                     $("#" + k + "Number").html(date[k]);
                 }
+                break;
+            case "tabData":
+                var data = obj[key];
+                var res = [];
+                if (data && data.length > 0) {
+                    data.map(function(item, index) {
+                        res.push(
+                            '<div class="checktab-item" data-index="item_' +
+                                index +
+                                '">' +
+                                '<div class="text">' +
+                                (index + 1) +
+                                "." +
+                                item.text +
+                                "</div>" +
+                                '<div class="checkbox">' +
+                                (item.isChecked
+                                    ? '<img src="./images/flag_check.png" />'
+                                    : "") +
+                                "</div>" +
+                                "</div>"
+                        );
+                    });
+                }
+                $("#checkBox").html(res.join(""));
+                break;
+            case "checkedData":
+                var data = obj[key];
+                var res = [];
+                if (data && data.length > 0) {
+                    data.map(function(item, index) {
+                        res.push(
+                            '<div class="checked-item" data-index="checkeditem_' +
+                                index +
+                                '">' +
+                                '<div class="text">' +
+                                item.text +
+                                "</div>" +
+                                '<div class="delete">x</div>' +
+                                "</div>"
+                        );
+                    });
+                }
+                $("#checkedBox").html(res.join(""));
                 break;
         }
     }
@@ -66,5 +143,44 @@ $(function() {
             renderObj.date = setTime();
         }.bind(this, 1000)
     );
-    renderObj.show = "step2";
+    $("#checkBox").on("click", ".checktab-item", function() {
+        var _me = $(this);
+        var _idx = _me.attr("data-index").split("_")[1];
+        var list = renderObj.tabData;
+        var checkList = renderObj.checkedData;
+        list[_idx].isChecked = !list[_idx].isChecked;
+        var newCheckList = [];
+        if (!list[_idx].isChecked) {
+            newCheckList = checkList.filter(function(item) {
+                if (item.text != list[_idx].text) {
+                    return item;
+                }
+            });
+        } else {
+            checkList.push(list[_idx]);
+            newCheckList = checkList;
+        }
+        $("#checkedNumber").html(newCheckList.length);
+        renderObj.tabData = JSON.parse(JSON.stringify(list));
+        renderObj.checkedData = JSON.parse(JSON.stringify(newCheckList));
+    });
+    $("#tabHeader").on("click", function() {
+        if (renderObj.showTab == "tab1") {
+            renderObj.showTab = "tab2";
+            $("#step2 .top").addClass("show");
+        } else {
+            renderObj.showTab = "tab1";
+            $("#step2 .top").removeClass("show");
+        }
+    });
+    $("#checkedBox").on("click", ".delete", function() {
+        var _me = $(this);
+        var _idx = _me.attr("data-index");
+        let list = renderObj.checkedData;
+        list.splice(_idx, 0);
+        renderObj.checkedData = JSON.parse(JSON.stringify(list));
+    });
+    $("#goStep2").on("click", function() {
+        renderObj.show = "step2";
+    });
 });
