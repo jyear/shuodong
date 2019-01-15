509d71307194d9b68ea9042097f466c1cc91c22a
yx53074 <yx53074@163.com>
2019-01-15 18:47:16 +0800

    2222

--- js/flag.js
+++ js/flag.js
@@ -27,32 +27,33 @@ var renderObj = {
             id: 0
         },
         {
-            text: "张三丰",
+            text: "张三丰1",
             isChecked: false,
             id: 0
         },
         {
-            text: "张三丰",
+            text: "张三丰2",
             isChecked: false,
             id: 0
         },
         {
-            text: "张三丰",
+            text: "张三丰3",
             isChecked: false,
             id: 0
         },
         {
-            text: "张三丰",
+            text: "张三丰4",
             isChecked: false,
             id: 0
         },
         {
-            text: "张三丰",
+            text: "张三丰5",
             isChecked: false,
             id: 0
         }
     ],
-    checkedData: []
+    checkedData: [],
+    userName: ""
 };
 var newObject = {};
 var lookRenderObj = function() {
@@ -100,20 +101,30 @@ var lookRenderObj = function() {
             case "checkedData":
                 var data = obj[key];
                 var res = [];
+                var imgList = [];
                 if (data && data.length > 0) {
                     data.map(function(item, index) {
                         res.push(
-                            '<div class="checked-item" data-index="checkeditem_' +
-                                index +
-                                '">' +
+                            '<div class="checked-item">' +
                                 '<div class="text">' +
                                 item.text +
                                 "</div>" +
-                                '<div class="delete">x</div>' +
+                                '<div class="delete" data-index="checkeditem_' +
+                                index +
+                                '">x</div>' +
+                                "</div>"
+                        );
+                        imgList.push(
+                            '<div class="item">' +
+                                (index + 1) +
+                                "." +
+                                item.text +
                                 "</div>"
                         );
                     });
                 }
+                $("#FlagList").html(imgList.join(""));
+                $("#checkedNumber").html(data.length);
                 $("#checkedBox").html(res.join(""));
                 break;
         }
@@ -143,6 +154,7 @@ $(function() {
             renderObj.date = setTime();
         }.bind(this, 1000)
     );
+    //renderObj.show = "step3";
     $("#checkBox").on("click", ".checktab-item", function() {
         var _me = $(this);
         var _idx = _me.attr("data-index").split("_")[1];
@@ -160,7 +172,7 @@ $(function() {
             checkList.push(list[_idx]);
             newCheckList = checkList;
         }
-        $("#checkedNumber").html(newCheckList.length);
+
         renderObj.tabData = JSON.parse(JSON.stringify(list));
         renderObj.checkedData = JSON.parse(JSON.stringify(newCheckList));
     });
@@ -175,12 +187,107 @@ $(function() {
     });
     $("#checkedBox").on("click", ".delete", function() {
         var _me = $(this);
-        var _idx = _me.attr("data-index");
+        var _idx = _me.attr("data-index").split("_")[1];
         let list = renderObj.checkedData;
-        list.splice(_idx, 0);
+        list.splice(_idx, 1);
         renderObj.checkedData = JSON.parse(JSON.stringify(list));
     });
     $("#goStep2").on("click", function() {
         renderObj.show = "step2";
+    });
+    $("#customerAddBtn").on("click", function() {
+        var _input = $("#inputBox");
+        var _value = _input[0].value;
+        if (_value.trim().length <= 0) {
+            return;
+        }
+        var list = renderObj.checkedData;
+        var isHave = false;
+        for (var i = 0, l = list.length; i < l; i++) {
+            if (list[i].text == _value.trim()) {
+                isHave = true;
+                break;
+            }
+        }
+        if (isHave) {
+            return;
+        }
+
+        list.push({
+            text: _value.trim()
+        });
+        renderObj.checkedData = JSON.parse(JSON.stringify(list));
+    });
+    //生成图片
+    $("#saveBtn").on("click", function() {
+        var saveBox = $("#saveBox");
+        $("html,body").css({
+            overflow: "hidden"
+        });
+        var imglayer = $("#imgLayerBox");
+        saveBox.addClass("show");
+        if (imglayer.html().length > 0) {
+            return;
+        }
+        html2canvas(document.querySelector("#willRenderImage"), {
+            allowTaint: true,
+            async: false,
+            backgroundColor: "#f0dec1",
+            scale: 3,
+            dpi: window.devicePixelRatio * 2
+        }).then(function(canvas) {
+            var context = canvas.getContext("2d");
+            context.mozImageSmoothingEnabled = false;
+            context.webkitImageSmoothingEnabled = false;
+            context.msImageSmoothingEnabled = false;
+            context.imageSmoothingEnabled = false;
+            var height = canvas.height;
+            var width = canvas.width;
+            context.clearRect(0, height - 2, width, height);
+            var img = new Image();
+            img.src = canvas.toDataURL("image/png");
+            img.alt = "解签";
+            img.className = "canvasimg";
+            img.onload = function() {
+                document.querySelector("#imgLayerBox").innerHTML = "";
+                document.querySelector("#imgLayerBox").appendChild(img);
+            };
+        });
+    });
+    $("#saveBox").on("click", function() {
+        $(this).removeClass("show");
+        $("html,body").css({
+            overflow: "auto"
+        });
+    });
+    $("#shareBtn").on("click", function() {
+        $("#shareLayer").addClass("show");
+    });
+    $("#shareLayer").on("click", function() {
+        $(this).removeClass("show");
+    });
+    $("#step3Btn").on("click", function() {
+        var _value = $("#userNameInput")[0].value.trim();
+        if (_value.length <= 0) {
+            return;
+        }
+        renderObj.show = "step5";
+        renderObj.userName = _value;
+        $("#userNameText").html(renderObj.userName);
+        var date = new Date();
+        var year = date.getFullYear();
+        var month = date.getMonth() + 1;
+        var day = date.getDate();
+        var hours = date.getHours();
+        var mins = date.getMinutes();
+        $("#flagTime").html(
+            "立于" + year + "/" + month + "/" + day + "/ " + hours + ":" + mins
+        );
+    });
+    $("#step2Btn").on("click", function() {
+        if (renderObj.checkedData.length <= 0) {
+            return;
+        }
+        renderObj.show = "step3";
     });
 });
---
+++ js/summary.js
@@ -0,0 +1,201 @@
+var RenderData = (function($) {
+    var defaultOpts = {
+        data: {}
+    };
+    var result = {};
+    var newResult = {};
+    function renderDataFunction(opts) {
+        this.opts = $.extend({}, defaultOpts, opts);
+        this.init.call(this);
+        return newResult;
+    }
+    function objectHandler(obj, key) {
+        switch (key) {
+            case "show":
+                $(".page").removeClass("show");
+                $("#" + obj[key]).addClass("show");
+                break;
+            default:
+                break;
+        }
+    }
+    renderDataFunction.prototype = {
+        constructor: renderData,
+        init: function() {
+            var data = this.opts.data;
+            result = JSON.parse(JSON.stringify(data));
+            Object.getOwnPropertyNames(result).forEach(function(k) {
+                objectHandler(result, k);
+                Object.defineProperty(newResult, k, {
+                    get: function() {
+                        return result[k];
+                    },
+                    set: function(value) {
+                        result[k] = value;
+                        objectHandler(result, k);
+                    }
+                });
+            });
+        }
+    };
+    return renderDataFunction;
+})($);
+
+var renderData = new RenderData({
+    data: {
+        show: "step1"
+    }
+});
+var countStep1Btn = function() {
+    var w = document.body.clientWidth;
+    var h = document.body.clientHeight;
+    var start_x = (w / 750) * 270;
+    var end_x = (w / 750) * 710;
+    var start_y = (h / 1323) * 1140;
+    var end_y = (h / 1323) * 1275;
+    $("#goStep2Btn").attr(
+        "coords",
+        start_x + "," + start_y + "," + end_x + "," + end_y
+    );
+};
+var goStep2Btn = function() {
+    renderData.show = "step2";
+};
+var timer = null;
+Array.prototype.distinct = function() {
+    var arr = this,
+        i,
+        j,
+        len = arr.length;
+    for (i = 0; i < len; i++) {
+        for (j = i + 1; j < len; j++) {
+            if (arr[i] == arr[j]) {
+                arr.splice(j, 1);
+                len--;
+                j--;
+            }
+        }
+    }
+    return arr;
+};
+$(function() {
+    //renderData.show = "step3";
+    countStep1Btn();
+    $("#step2").on("click", function() {
+        $(".group-item")
+            .attr("contentEditable", false)
+            .removeClass("edit");
+    });
+    $("#groupContainer").on("click", ".group-item", function() {
+        $(this).toggleClass("cur");
+    });
+    $("#groupContainer").on("touchstart", ".group-item", function(e) {
+        e.stopPropagation();
+        if (!timer) {
+            timer = setTimeout(
+                function() {
+                    $(this).attr("contentEditable", true);
+                    $(this)
+                        .addClass("edit")
+                        .addClass("cur");
+                }.bind(this),
+                1000
+            );
+        }
+    });
+    $("#groupContainer").on("touchend", ".group-item", function(e) {
+        e.stopPropagation();
+        if (timer) {
+            clearTimeout(timer);
+            timer = null;
+        } else {
+            $(this).toggleClass("cur");
+        }
+    });
+    $("#groupContainer").on("click", ".group-add", function() {
+        var str = '<div class="group-item">长按输入内容</div>';
+        $(str).insertBefore($(this));
+    });
+    $("#goStep3Btn").on("click", function() {
+        var list = $("#groupContainer .group-item.cur");
+        var stringArr = [];
+        var textArr = [];
+        list.each(function(index, item) {
+            var text = item.innerHTML;
+            stringArr.push(text);
+        });
+        if (!stringArr || stringArr.length <= 0) {
+            return;
+        }
+        stringArr.distinct().map(function(item, index) {
+            textArr.push(
+                '<div class="summary-item">' +
+                    '<div class="st">' +
+                    (index + 1) +
+                    "." +
+                    item +
+                    "</div>" +
+                    '<div class="si">' +
+                    ' <img src="./images/summary_s3.png" />' +
+                    " </div>" +
+                    "</div>"
+            );
+        });
+        $("#innerList").html(textArr.join(""));
+        renderData.show = "step3";
+    });
+    var h = document.body.clientHeight;
+    $("#innerBox").css("min-height", h - 150 + "px");
+    //生成图片
+    $("#saveBtn").on("click", function(e) {
+        e.stopPropagation();
+        var saveBox = $("#saveBox");
+        $("html,body").css({
+            overflow: "hidden"
+        });
+        var imglayer = $("#imgLayerBox");
+        saveBox.addClass("show");
+        if (imglayer.html().length > 0) {
+            return;
+        }
+        html2canvas(document.querySelector("#willRenderImage"), {
+            allowTaint: true,
+            async: false,
+            backgroundColor: "#f0dec1",
+            scale: 3,
+            dpi: window.devicePixelRatio * 2
+        }).then(function(canvas) {
+            var context = canvas.getContext("2d");
+            context.mozImageSmoothingEnabled = false;
+            context.webkitImageSmoothingEnabled = false;
+            context.msImageSmoothingEnabled = false;
+            context.imageSmoothingEnabled = false;
+            var height = canvas.height;
+            var width = canvas.width;
+            context.clearRect(0, height - 2, width, height);
+            var img = new Image();
+            img.src = canvas.toDataURL("image/png");
+            img.alt = "解签";
+            img.className = "canvasimg";
+            img.onload = function() {
+                document.querySelector("#imgLayerBox").innerHTML = "";
+                document.querySelector("#imgLayerBox").appendChild(img);
+            };
+        });
+    });
+    $("#saveBox").on("click", function(e) {
+        e.stopPropagation();
+        $(this).removeClass("show");
+        $("html,body").css({
+            overflow: "auto"
+        });
+    });
+    $("#shareBtn").on("click", function(e) {
+        e.stopPropagation();
+        $("#shareLayer").addClass("show");
+    });
+    $("#shareLayer").on("click", function(e) {
+        e.stopPropagation();
+        $(this).removeClass("show");
+    });
+});
